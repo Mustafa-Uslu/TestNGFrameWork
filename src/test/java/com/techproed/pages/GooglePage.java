dd075c2e2acb373861db6761106d9de3e4a8be24
Mustafa-Uslu <Mustafauslu.de@gmail.com>
2020-09-18 22:42:51 +0200

    Freamwork

---
+++ src/test/java/com/techproed/pages/GooglePage.java
@@ -0,0 +1,17 @@
+package com.techproed.pages;
+
+import com.techproed.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Factory;
+
+public class GooglePage {
+    public GooglePage(){
+        PageFactory.initElements(Driver.getDriver() , this);
+    }
+    @FindBy (  partialLinkText = "Gmail" )
+    public WebElement gmailLinki;
+
+
+}
